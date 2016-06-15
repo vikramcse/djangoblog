2e2654516c7150a7426e373f0dc3e2b311902fac
vikram jadhav <vikramcse.10@gmail.com>
2016-06-15 14:17:41 +0530

    added local auth

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name="post_list"),
+    url(r'^login$', views.log_in, name="login"),
+    url(r'^logout$', views.log_out, name="logout"),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name="post_detail"),
     url(r'^post/new/$', views.post_new, name="post_new"),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name="post_edit"),
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.contrib.auth import authenticate, login, logout
 
 from .forms import PostForm
 
@@ -7,11 +8,35 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now())
+    errors = []
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            if user.is_active:
+                print "logged in by " + user.username
+                login(request, user)
+            else:
+                erros.push('account disabled :(')
+        else:
+            erros.push('Invalid Login')
+
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def log_in(request):
+    return render(request, 'blog/login.html')
+
+def log_out(request):
+    logout(request)
+    return render(request, 'blog/login.html')
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
