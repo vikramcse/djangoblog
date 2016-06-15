b2473546a5347b03472604e249e5e28c10a72e86
vikram jadhav <vikramcse.10@gmail.com>
2016-06-15 10:02:08 +0530

    added templates and viewes and renderd the blogs on page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
 
-# Create your views here.
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
